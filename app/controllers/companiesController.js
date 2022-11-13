bcfc8e7669782a7786f61fa82e3ef5bd37e16005
M G <martaxgarijo@gmail.com>
2022-11-13 13:42:45 +0100

    Base de dades connecta i els dos primers endpoints també

--- app/controllers/companiesController.js
+++ app/controllers/companiesController.js
@@ -3,24 +3,24 @@ const Companyia = require('../models/modelCompanies.js');
 
 
 const companyiesOrdenadesPerTamany = async (req, res) => {
-    let llistat = "";
+    // let llistat = "";
 
     try {
         const llistatOrdenatPerTamany = await Companyia.find().sort({ size: -1 });
 
-        res.status(200).send(llistat);
+        res.status(200).send(llistatOrdenatPerTamany);
     } catch (error) {
         res.status(400).send(error);
     }
 };
 
 const companyiesOrdenadesPerData = async (req, res) => {
-    let llistat = "";
+    // let llistat = "";
 
     try {
-        const llistatOrdenatPerTamany = await Companyia.find().sort({ founded: 1 });
+        const llistatOrdenatPerData = await Companyia.find().sort({ founded: 1 });
 
-        res.status(200).send(llistat);
+        res.status(200).send(llistatOrdenatPerData);
     } catch (error) {
         res.status(400).send(error);
     }
@@ -32,7 +32,7 @@ const quantitatEmpreses = async (req, res) => {
     
     try {
         // Número de empresas que hay en cada industria, 
-        const empresesPerIndustria = await Companyia.count({ group: industry });
+        const empresesPerIndustria = await Companyia.aggregate({ group: industry });
         
         // Número de empresas que hay por cada rango de tamaños, 
         const empresesPerTamany = await Companyia.count({group: size});
@@ -40,7 +40,7 @@ const quantitatEmpreses = async (req, res) => {
         // Número de empresas que hay en cada año de creación
         const empresesPerAny = await Companyia.count({group: founded});
 
-        res.status(200).send(missatge);
+        res.status(200).send("Nombre d'empreses per cada industria:", empresesPerIndustria);
     } catch (error) {
         res.status(400).send(error);
     }
